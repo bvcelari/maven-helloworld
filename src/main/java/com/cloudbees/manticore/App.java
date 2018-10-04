a52fe8e212da99f08b891736f23a93b7ca502b9e
Antonio Muniz <amuniz@users.noreply.github.com>
2018-10-04 11:28:53 +0200

    Update App.java

--- src/main/java/com/cloudbees/manticore/App.java
+++ src/main/java/com/cloudbees/manticore/App.java
@@ -11,8 +11,7 @@ public class App
         String a = null;
         System.out.println(a.toString());
         System.out.println( "Hello World!" );
-        // TODO: this is a task
-        // FIXME: this is another task 
+
         try {
             a = "Hello";
         } catch (Exception e) {}
